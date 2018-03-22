52825c31773ab1735d035f9d1b7fb91bd63a53c6
tessie <tessyjoseph1992@gmail.com>
2018-03-22 15:47:08 +0530

    Setting up basic test

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Razorpay_Woocommerce_Subscriptions
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
